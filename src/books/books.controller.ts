ff67bcd627c142c841d27de1ba602a4af3038ce2
MalibyTM <rahimg0319@gmail.com>
2025-02-04 12:14:58 +0500

    feat: Добавил функцию для image в books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,15 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/books.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname, join } from 'path';
+import { diskStorage } from 'multer';
 
 @Controller('books')
 export class BooksController {
     constructor(private booksService: BooksService){}
-
-    @Post()
-    createBook(@Body() dto: CreateBookDto){
-        return this.booksService.createBook(dto);
-    }
 
     @Get()
     getAllBooks(){
@@ -25,4 +23,39 @@ export class BooksController {
     deleteBook(@Param('id') id: number){
         return this.booksService.deleteBook(id);
     }
+
+    @Post()
+    @UseInterceptors(FileInterceptor('image', {
+        storage: diskStorage({
+            destination: join(__dirname, '..', 'uploads'),
+            filename: (req, file, cb) => {
+                const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + extname(file.originalname);
+                cb(null, uniqueName);
+            }
+        })
+    }))
+    async uploadBook(
+        @UploadedFile() file: Express.Multer.File,
+        @Body() dto: CreateBookDto
+    ) {
+        console.log('Received Data:', dto); // 🔍 Проверяем входные данные
+    
+        try {
+            const parsedAuthor = JSON.parse(dto.author);
+            const parsedContent = JSON.parse(dto.content);
+    
+            const imagePath = `/uploads/${file.filename}`;
+    
+            return this.booksService.createBook({
+                ...dto,
+                author: parsedAuthor,
+                content: parsedContent,
+                image: imagePath
+            });
+    
+        } catch (error) {
+            throw new Error('Ошибка парсинга JSON: Проверь данные author и content');
+        }
+    }
+    
 }
--- src/books/books.model.ts
+++ src/books/books.model.ts
@@ -12,12 +12,21 @@ export class Book extends Model<Book, BookCreationAttrs> {
     @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true, unique: true, })
     id: number;
 
-    @Column({ type: DataType.STRING, allowNull: false })
-    author: string;
+    @Column({ 
+        type: DataType.JSONB, 
+        allowNull: false 
+    })
+    author: Record<string, string>;
 
-    @Column({ type: DataType.TEXT, allowNull: false })
-    content: string;
+    @Column({ 
+        type: DataType.JSONB, 
+        allowNull: false 
+    })
+    content: Record<string, string>;
 
-    @Column({ type: DataType.STRING, allowNull: true })
+    @Column({ 
+        type: DataType.STRING, 
+        allowNull: false 
+    })
     image: string;
 }
